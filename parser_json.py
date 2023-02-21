d3e8b69065d9a7088aca4515d808edb07cdd70f6
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-02-21 22:40:51 +0200

    Add the words in json format

---
+++ parser_json.py
@@ -0,0 +1,15 @@
+import json
+import re
+
+INPUT_FILE = "kotus-sanalista_v1.xml"
+OUTPUT_FILE = "kotus-sanalista.json"
+MATCH_STRING = r"(?<=<s>).*(?=<\/s>)"  # match everything between <s> and </s>
+
+with open(INPUT_FILE, encoding="utf-8") as f:
+    contents = f.readlines()
+
+words = [re.search(MATCH_STRING, line).group() for line in contents if "<s>" in line and "</s>" in line]
+
+with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+    json.dump(words, f, indent=2, ensure_ascii=False)
+
