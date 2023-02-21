4271c31ede06b906309fb9692ff29be12e754203
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-02-21 20:59:45 +0200

    Add parser to extract just the words from xml

---
+++ parser.py
@@ -0,0 +1,13 @@
+import re
+
+INPUT_FILE = "kotus-sanalista_v1.xml"
+OUTPUT_FILE = "kotus-sanalista.txt"
+MATCH_STRING = r"(?<=<s>).*(?=<\/s>)"  # match everything between <s> and </s>
+
+with open(INPUT_FILE, encoding="utf-8") as f:
+    contents = f.readlines()
+
+words = [re.search(MATCH_STRING, line).group() + "\n" for line in contents if "<s>" in line and "</s>" in line]
+
+with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+    f.writelines(words)
