bc84854c7f149c4e2e1c7459e4967d5d71b74eab
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-02-22 13:09:38 +0200

    Add an xml parser

--- parser_xml.py
+++ parser_xml.py
@@ -1,15 +1,32 @@
-import re
+import os
+import xml.etree.ElementTree as ET
 
-INPUT_FILE = "murre/osa1/01_a.xml"
-OUTPUT_FILE = "kotus-sanalista-murteet.txt"
-# MATCH_STRING = r"(?<=<s>).*(?=<\/s>)"  # match everything between <s> and </s>
-MATCH_STRING = r"(?<=<Headword>).*(?=<\/Headword>)"  # match everything between <s> and </s>
+INPUT_FILE_DIRECTORY = "murre"
+OUTPUT_FILE = "kotus-sanalista-murre.txt"
 
-with open(INPUT_FILE, encoding="utf-8") as f:
-    contents = f.read()
+xml_files = []
+for root, dirs, files in os.walk(INPUT_FILE_DIRECTORY, topdown=False):
+    for name in files:
+        if name.endswith(".xml"):
+            xml_files.append(os.path.join(root, name))
 
-matches = re.findall(MATCH_STRING, contents)
+words = []
 
+for xml_file in xml_files:
+    new_words = []
+    print(f"Processing {xml_file}")
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
 
+    for dict_entry in root.findall('DictionaryEntry'):
+        headword_container = dict_entry.find('HeadwordCtn')
+        headword = headword_container.find('Headword')
+        headword_text = headword.text.replace("|", "") + "\n"
+        new_words.append(headword_text)
+
+    print(f"New words found: {len(new_words)}")
+    words += new_words
+
+print(f"Processing complete, writing {len(words)} words to {OUTPUT_FILE}")
 with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
-    f.writelines(words)
+    f.writelines(sorted(words))
